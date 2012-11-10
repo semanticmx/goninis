caf489ad8aaea72477a0abbb2eab546195ed135b
Carlos Vences <carlos@semantic.mx>
2012-11-10 16:35:07 -0600

    CV, design v1

--- apps/landing/modules/optin/templates/showSuccess.php
+++ apps/landing/modules/optin/templates/showSuccess.php
@@ -1,14 +1,15 @@
 <div class="container_24">
-  <div class="logo">&nbsp;</div>
-  <div class="optin popup">
+  <div class="logo grid_1">&nbsp;</div>
+  <div class="grid_7 optin popup">
     <?php echo form_tag ('optin/register')?>
       <?php echo input_tag('email')?>
       <?php echo input_tag('zipcode')?>
       <?php echo submit_image_tag('/images/landing/sprite_unete_118x61.png')?>
     </form>
   </div>
-  <div class="icon facebook">&nbsp;</div>
-  <div class="icon twitter">&nbsp;</div>
-  <div class="icon linkedin">&nbsp;</div>
-  <div class="icon pinterest">&nbsp;</div>
+  <div class="clearfix">&nbsp;</div>
+  <div class="grid_1 icon facebook">&nbsp;</div>
+  <div class="grid_3 icon twitter">&nbsp;</div>
+  <div class="grid_5 icon linkedin">&nbsp;</div>
+  <div class="grid_7 icon pinterest">&nbsp;</div>
 </div>
