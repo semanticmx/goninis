7735859324a53f1bf95244f53f5be90a1bc85fa7
Carlos Vences <carlos@semantic.mx>
2012-11-10 18:46:28 -0600

    CV, fix validation

--- apps/landing/modules/optin/actions/actions.class.php
+++ apps/landing/modules/optin/actions/actions.class.php
@@ -22,20 +22,24 @@ class optinActions extends sfActions
   }
   public function executeRegister ()
   {
-    require_once dirname(__FILE__).'/../../../config/config.inc.php';
-    $api = new MCAPI ($apikey);
-    $merge_vars = array(
-      'FNAME'=>'Test', 
-      'LNAME'=>'Account',
-      'ZIPCODE'=>$this->getRequestParameter ('zipcode')
-    );
-    $retval = $api->listSubscribe( $listId, $this->getRequestParameter ('email'), $merge_vars );
-
-    if ($api->errorCode) {
+    if (!$this->getRequestParameter ('email') || !$this->getRequestParameter ('zipcode') || !is_numeric($this->getRequestParameter ('zipcode')))
       $this->classname = "error";
-    } else {
-      $this->classname = "ok";
-    }    
+    else{
+      require_once dirname(__FILE__).'/../../../config/config.inc.php';
+      $api = new MCAPI ($apikey);
+      $merge_vars = array(
+        'FNAME'=>'Test', 
+        'LNAME'=>'Account',
+        'ZIPCODE'=>$this->getRequestParameter ('zipcode')
+      );
+      $retval = $api->listSubscribe( $listId, $this->getRequestParameter ('email'), $merge_vars );
+
+      if ($api->errorCode) {
+        $this->classname = "error";
+      } else {
+        $this->classname = "ok";
+      }
+    }
   }
   public function executeShow() 
   {
--- apps/landing/modules/optin/templates/registerSuccess.php
+++ apps/landing/modules/optin/templates/registerSuccess.php
@@ -1,6 +1,6 @@
 <div class="logo grid_1">&nbsp;</div>
 <div class="grid_7 optin popup">
-  <div class="feedback <?php echo $classname; ?>"></div>
+  <a href="javascript:history.back();"><div class="feedback <?php echo $classname; ?>"></div></a>
 </div>
 <div class="clearfix">&nbsp;</div>
 <div class="grid_1 icon facebook">&nbsp;</div>
