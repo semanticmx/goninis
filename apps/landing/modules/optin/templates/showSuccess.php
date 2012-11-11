c9a3da7e742c03edcca2935e6f0d1af5496a953e
Carlos Vences <carlos@semantic.mx>
2012-11-10 20:41:09 -0600

    CV, fix footer padding and links

--- apps/landing/modules/optin/templates/showSuccess.php
+++ apps/landing/modules/optin/templates/showSuccess.php
@@ -8,7 +8,9 @@
   </form>
 </div>
 <div class="clearfix">&nbsp;</div>
-<div class="grid_1 icon facebook">&nbsp;</div>
-<div class="grid_3 icon twitter">&nbsp;</div>
-<div class="grid_5 icon linkedin">&nbsp;</div>
-<div class="grid_7 icon pinterest">&nbsp;</div>
+<div class="push_1 grid_11">
+  <a href="http://www.facebook.com/pages/GoNinis/215959698428623" target="_blank"><div class="grid_1 icon facebook">&nbsp;</div></a>
+  <a href="https://twitter.com/goninis" target="_blank"><div class="grid_3 icon twitter">&nbsp;</div></a>
+  <a href="#"  target="_blank"><div class="grid_5 icon linkedin">&nbsp;</div></a>
+  <a href="#" target="_blank"><div class="grid_7 icon pinterest">&nbsp;</div></a>
+</div>
